e28585574b7639bd9781466406753562d8aa65b1
Abzaparov-Nikolay <nd.abzaparov@gmail.com>
2022-07-08 21:40:55 +0500

    Добавлена вибрация обоих контроллеров

--- VR Feedback/Assets/VibrationController.cs
+++ VR Feedback/Assets/VibrationController.cs
@@ -7,10 +7,19 @@ namespace VrFeedback
     {
         private void OnTriggerEnter(Collider collider)
         {
-            var handCube = collider.gameObject;
-            var controller = handCube.GetComponentInParent<ActionBasedController>();
-            controller.SendHapticImpulse(0.7f, 2);
-            Debug.Log($"Sent vibration to {controller.name}");
+            if(gameObject.tag == "Sphere")
+            {
+                var handCube = collider.gameObject;
+                var controller = handCube.GetComponentInParent<ActionBasedController>();
+                controller.SendHapticImpulse(0.7f, 2);
+                Debug.Log($"Sent vibration to {controller.name}");
+            }
+            if(gameObject.tag == "Cylinder")
+            {
+                GameController.LeftController.SendHapticImpulse(0.7f, 2);
+                GameController.RightController.SendHapticImpulse(0.7f, 2);
+                Debug.Log("Vibration sent to both controllers");
+            }
         }
     }
 }
